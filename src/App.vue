076316ec6bd5c598b0b61c979c986a0e7bb3c882
waltereidi <walter-eidi@hotmail.com>
2023-09-18 21:30:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,208 @@
+<script >
+export default {
+
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="container--header">
+      <div class="container--header__menu">
+        <a href="#">Website Animado</a>
+
+      </div>
+      <div class="container--header__menu">
+        <a href="#">Outro lado do site</a>
+
+      </div>
+      <div class="container--header__menu-right">
+        <a href="#">Ticaracatica</a>
+
+      </div>
+
+    </div>
+    <div class="container--banner">
+
+
+    </div>
+    <div class="container--divisoria">
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+      <div class="item">TICARACATICA</div>
+
+    </div>
+    <div class="container--content">
+      <div class="container--content__leftBar">
+        <div class="menuItem">
+          <p>O MENU</p>
+        </div>
+        <div class="menuItem">
+          <p>ITEM DO MENU</p>
+        </div>
+        <div class="menuItem">
+          <p>MENU ITEM</p>
+        </div>
+        <div class="menuItem">
+          <p>MENYU AITEMU</p>
+        </div>
+
+
+      </div>
+      <div class="container--content__mainContent">
+        <div class="grid-item">Acredite em você!</div>
+        <div class="grid-item">Nunca desista!</div>
+        <div class="grid-item">Você é capaz.</div>
+        <div class="grid-item">Seja persistente.</div>
+        <div class="grid-item">Sonhe alto.</div>
+        <div class="grid-item">Supere obstáculos.</div>
+        <div class="grid-item">Viva intensamente.</div>
+        <div class="grid-item">Seja resiliente.</div>
+        <div class="grid-item">Sorria sempre.</div>
+        <div class="grid-item">Faça acontecer.</div>
+        <div class="grid-item">Vá além!</div>
+        <div class="grid-item">Seja otimista.</div>
+        <div class="grid-item">Evolua sempre.</div>
+        <div class="grid-item">Foque no bem.</div>
+        <div class="grid-item">Seja gentil.</div>
+        <div class="grid-item">Lute com garra.</div>
+        <div class="grid-item">Supere medos.</div>
+        <div class="grid-item">Busque seu sonho.</div>
+        <div class="grid-item">Seja grato.</div>
+        <div class="grid-item">Vença desafios.</div>
+        <div class="grid-item">Seja corajoso.</div>
+        <div class="grid-item">Faça a diferença.</div>
+        <div class="grid-item">Seja inspiração.</div>
+        <div class="grid-item">Viva com paixão.</div>
+        <div class="grid-item">Mire nas estrelas.</div>
+        <div class="grid-item">Aja com amor.</div>
+        <div class="grid-item">Ouse ser grande.</div>
+        <div class="grid-item">Seja único.</div>
+        <div class="grid-item">Confie em si.</div>
+        <div class="grid-item">Não se renda.</div>
+        <div class="grid-item">Seja resiliente.</div>
+        <div class="grid-item">Mantenha a fé.</div>
+        <div class="grid-item">Brilhe sempre.</div>
+        <div class="grid-item">Persiga metas.</div>
+        <div class="grid-item">Seja determinado.</div>
+        <div class="grid-item">Ame a jornada.</div>
+        <div class="grid-item">Faça história.</div>
+        <div class="grid-item">Nunca recue.</div>
+        <div class="grid-item">Cultive a paz.</div>
+        <div class="grid-item">Esteja presente.</div>
+        <div class="grid-item">Seja generoso.</div>
+        <div class="grid-item">Inspire outros.</div>
+        <div class="grid-item">Busque a verdade.</div>
+        <div class="grid-item">Enfrente o medo.</div>
+        <div class="grid-item">Seja valente.</div>
+        <div class="grid-item">Seja autêntico.</div>
+        <div class="grid-item">Crie sua sorte.</div>
+        <div class="grid-item">Seja um herói.</div>
+        <div class="grid-item">Viva o agora.</div>
+        <div class="grid-item">Supere limites.</div>
+        <div class="grid-item">Seja persistente.</div>
+        <div class="grid-item">Abrace a vida.</div>
+        <div class="grid-item">Seja imparável.</div>
+        <div class="grid-item">Abra portas.</div>
+        <div class="grid-item">Seja ousado.</div>
+        <div class="grid-item">Alcance o topo.</div>
+        <div class="grid-item">Nunca desista!</div>
+        <div class="grid-item">Seja resiliente.</div>
+        <div class="grid-item">Siga seu coração.</div>
+        <div class="grid-item">Seja extraordinário.</div>
+        <div class="grid-item">Ame a si mesmo.</div>
+        <div class="grid-item">Espalhe sorrisos.</div>
+        <div class="grid-item">Conquiste o mundo.</div>
+        <div class="grid-item">Viva seus sonhos!</div>
+
+      </div>
+    </div>
+    <div class="container--footer">
+
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+@import "assets/main.scss";
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
